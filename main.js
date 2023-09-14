ea7922a602c6d6d7b08fc1c700f81bdb5337227d
iamajraj <akmalraj07@gmail.com>
2023-09-15 00:10:40 +0600

    design complete

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const hamburger = document.querySelector('#hamburger');
+const navbar = document.querySelector('#navbar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
