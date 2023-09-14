897f19ce7c9da542bcb0b5704ff2d9d3605d31f1
iamajraj <akmalraj07@gmail.com>
2023-09-14 22:20:57 +0600

    features section and other sections added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         'secondary-dark': '#555',
         dark: '#0C0C0C',
         'secondary-white': 'rgba(255, 255, 255, 0.80)',
+        'feature-bg': 'rgba(12, 133, 133, 0.06)',
       },
     },
   },
