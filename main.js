2eb366574c5f12ea9e5565989db486fa9817ed91
iamajraj <akmalraj07@gmail.com>
2023-09-15 09:39:55 +0600

    aos and hover on effect added

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import './style.css';
+import 'aos/dist/aos.css';
+import Aos from 'aos';
 
 const hamburger = document.querySelector('#hamburger');
 const navbar = document.querySelector('#navbar');
 
 hamburger.addEventListener('click', () => {
   navbar.classList.toggle('active');
+});
+
+Aos.init({
+  mirror: false,
 });
