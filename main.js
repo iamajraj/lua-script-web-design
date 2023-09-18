bd05567f57564c7e3b5d63003fd8b249d7790957
iamajraj <akmalraj07@gmail.com>
2023-09-18 19:59:45 +0600

    stat counting animation function created based on the data-attribute of data-count and implemented to the index.html

--- main.js
+++ main.js
@@ -12,4 +12,30 @@ hamburger.addEventListener('click', () => {
 Aos.init({
   mirror: false,
   once: true,
+  disable: true,
 });
+
+// stat number count
+const countsEl = document.querySelectorAll('[data-count]');
+
+countsEl.forEach((el) => {
+  const numberToCount = el.getAttribute('data-count');
+  const suffix = el.getAttribute('data-count-suffix');
+  const ms = el.getAttribute('data-count-time');
+  countToN(Number(numberToCount), el, suffix, ms);
+});
+
+function countToN(n, el, suffix, ms) {
+  let clear;
+  let i = 0;
+  clear = setInterval(
+    () => {
+      if (i > n) {
+        return clearInterval(clear);
+      }
+      el.textContent = i + suffix;
+      i += 1;
+    },
+    ms ? Number(ms) : 50
+  );
+}
